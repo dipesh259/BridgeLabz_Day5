6bfacc025645f041808d974309e6eda0daa9f972
sick3356 <myselfdipu259@gmail.com>
2021-09-11 11:22:03 +0530

    Vowel or Consonant

--- Solution.java
+++ Solution.java
@@ -1,22 +1,19 @@
-				//Find Odd or Even
+				//Check for Vowel or Consonant
 
 package learned;
 
 import java.util.Scanner;
 
-public class OddorEven {
+public class VowelsConsonent {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a value: ");
-        int num = sc.nextInt();
+        System.out.println("Enter an alphabet");
+        char alpha = sc.next().charAt(0);
 
-        if (num%2==0)
-            System.out.println("It is even");
+        if ( alpha == 'a' || alpha == 'e' || alpha == 'i' || alpha == 'u')
+            System.out.println("It is vowel");
         else
-            System.out.println("It is odd");
-
-
-
+            System.out.println("It is a consonant");
     }
 }
