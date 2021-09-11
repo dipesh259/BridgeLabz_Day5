a0f5e4e96c75ec58688ee4d1491cf05e3e35a2b5
sick3356 <myselfdipu259@gmail.com>
2021-09-11 10:29:28 +0530

    Leap Year

--- Solution.java
+++ Solution.java
@@ -1,12 +1,17 @@
 package learned;
 
-public class Flip {
-    public static void main(String[] args) {
-        if(Math.random() < 0.5) {
-            System.out.println("Heads");
-        }
-        else{
-                System.out.println("Tails");
-            }
-        }
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args){
+        int year;
+        System.out.println("Enter an Year: ");
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+            System.out.println("It is a leap year");
+        else
+            System.out.println("It is not a leap year");
     }
+}
